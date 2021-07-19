28e84684d32ff4cf801ddf111719fd04654228cc
jaesungkr <kr.jaesung@gmail.com>
2021-07-19 22:38:56 +0900

    beautiful_soup

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import pandas as pd
 import numpy as np
 import requests, re
 from bs4 import BeautifulSoup
+
+url = "https://www.kr.playblackdesert.com/Adventure/Guild/GuildProfile?guildName=%EC%9A%B0%EB%A6%AC%EC%99%80%EC%8D%A8%EB%98%90%EC%99%80%EC%8D%A8"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# print nicely
+# print(soup.prettify())
+
+unordered = soup.find_all("div", {"class": "container guild_profile"})
+my_list = soup.find_all("span", {"class": "text"})
+
+
+
+
+print(my_list)
+
