0dbaf957c3f1a1b24cfbcefd6f0a52b0e9146c7f
jaesungkr <kr.jaesung@gmail.com>
2021-07-20 01:27:25 +0900

    basic_function

--- main.py
+++ main.py
@@ -2,29 +2,27 @@ import pandas as pd
 import numpy as np
 import requests, re
 from bs4 import BeautifulSoup
+guild_name = '우리와써또와써'
+url = f"https://www.kr.playblackdesert.com/Adventure/Guild/GuildProfile?guildName={guild_name}"
 
-url = "https://www.kr.playblackdesert.com/Adventure/Guild/GuildProfile?guildName=%EC%9A%B0%EB%A6%AC%EC%99%80%EC%8D%A8%EB%98%90%EC%99%80%EC%8D%A8"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# print nicely
-# print(soup.prettify())
-my_list = []
+
+# Guild members extracting
+crew_list = []
 unordered = soup.find_all("div", {"class": "container guild_profile"})
 unordered_list = soup.find_all("span", {"class": "text"})
 unordered_list.pop(0)
 people_list = unordered_list
 #print(people_list[0])
 for x in people_list:
-    my_list.append(str(x))
+   #soup_set.append(str(x))
+    crew_list.append(re.compile('">([a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+)').findall(str(x)))
 
-# 아이디 추출
-a = re.compile('[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+').findall(my_list[0])
-print(a)
+# 길드원 수
+n_crew = len(crew_list)
+print(crew_list)
+print(n_crew)
 
-#print(re.findall('">[\', people_list[0]))
-
-
-#print(unordered)
-#print(len(my_list))
 
