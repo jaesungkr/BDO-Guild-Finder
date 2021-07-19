3b83cc687be30dfb65663355b8caa4bb29a3ad5d
jaesungkr <kr.jaesung@gmail.com>
2021-07-20 01:49:10 +0900

    saving to xlsx, reading xlsx file

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-import pandas as pd
-import numpy as np
 import requests, re
 from bs4 import BeautifulSoup
-guild_name = '우리와써또와써'
-url = f"https://www.kr.playblackdesert.com/Adventure/Guild/GuildProfile?guildName={guild_name}"
+import pandas as pd
+import openpyxl
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+def crew_crawling(guild_name):
 
+    url = f"https://www.kr.playblackdesert.com/Adventure/Guild/GuildProfile?guildName={guild_name}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# Guild members extracting
-crew_list = []
-unordered = soup.find_all("div", {"class": "container guild_profile"})
-unordered_list = soup.find_all("span", {"class": "text"})
-unordered_list.pop(0)
-people_list = unordered_list
-#print(people_list[0])
-for x in people_list:
-   #soup_set.append(str(x))
-    crew_list.append(re.compile('">([a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+)').findall(str(x)))
+    # Guild members extracting
+    crew_list = []
+    unordered_list = soup.find_all("span", {"class": "text"})
+    unordered_list.pop(0)
+    people_list = unordered_list
 
-# 길드원 수
-n_crew = len(crew_list)
-print(crew_list)
-print(n_crew)
+    for x in people_list:
+        crew_list.append(re.compile('">([a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+)').findall(str(x)))
+    # number of crew members
+    n_crew = len(crew_list)
+    return n_crew, crew_list
 
 
+def saving_list(crew_list):
+    df = pd.DataFrame(crew_list)
+    writer = pd.ExcelWriter('guild_info.xlsx', engine = 'xlsxwriter')
+    df.to_excel(writer, sheet_name='guild', index = False)
+    writer.save()
+
+
+def compare_list():
+    a = pd.read_excel('guild_info.xlsx', engine = 'openpyxl')
+    print(len(a[0]))
+
+def send_notification():
+
+
+
+
+guild = '우리와써또와써'
+length, my_list = crew_crawling(guild)
+saving_list(my_list)
+compare_list()
