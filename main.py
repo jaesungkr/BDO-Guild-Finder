e9a8c4678af0727e4f938493d8bd4dbcbb564fb0
jaesungkr <kr.jaesung@gmail.com>
2021-07-19 23:31:03 +0900

    extract user name successfully

--- main.py
+++ main.py
@@ -9,12 +9,22 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 # print nicely
 # print(soup.prettify())
-
+my_list = []
 unordered = soup.find_all("div", {"class": "container guild_profile"})
-my_list = soup.find_all("span", {"class": "text"})
+unordered_list = soup.find_all("span", {"class": "text"})
+unordered_list.pop(0)
+people_list = unordered_list
+#print(people_list[0])
+for x in people_list:
+    my_list.append(str(x))
+
+# 아이디 추출
+a = re.compile('[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+').findall(my_list[0])
+print(a)
+
+#print(re.findall('">[\', people_list[0]))
 
 
-
-
-print(my_list)
+#print(unordered)
+#print(len(my_list))
 
